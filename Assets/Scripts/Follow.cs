a9db2bb329e028b468e058cb6e6046f856a74abf
saintsugar <17062577+SaintSugar@users.noreply.github.com>
2022-11-25 08:44:33 +0300

    Fixed bug with infinite elasticity, when the camera was centered not on the nearest point on the circle, but on the target
    Rewritten control. Now it does not affect the application of external forces to the object
    
    Started development of jumps and collision detection

--- Assets/Scripts/Follow.cs
+++ Assets/Scripts/Follow.cs
@@ -13,35 +13,37 @@ public class Follow : MonoBehaviour {
     // Update is called once per frame
     void Update () {
         float xp, yp, xc, yc;
+        xp = Target.transform.position.x;
+        yp = Target.transform.position.y;
+
+        xc = transform.position.x;
+        yc = transform.position.y;
+
+        Vector2 pointer = new Vector2((xc-xp), (yc-yp)).normalized;
+        
+        float xo, yo;   
+
+        xo = pointer.x * Radius + xp;
+        yo = pointer.y * Radius + yp;
+
+
+
+        float dist;
+        dist = Mathf.Sqrt(Mathf.Pow(xp-xc, 2) + Mathf.Pow(yp-yc, 2));   
+        Vector2 V = GetComponent<Rigidbody2D>().velocity;
+        if (dist > Radius) {
+            V = new Vector2((xo-xc), (yo-yc))*Elasticity;
+        }
+        else {
+            V = new Vector2(0, 0);
+        }
         if (!InfititeElasticity) {
-            xp = Target.transform.position.x;
-            yp = Target.transform.position.y;
-
-            xc = transform.position.x;
-            yc = transform.position.y;
-
-            Vector2 pointer = new Vector2((xc-xp), (yc-yp)).normalized;
-            
-            float xo, yo;   
-
-            xo = pointer.x * Radius + xp;
-            yo = pointer.y * Radius + yp;
-
-
-
-            float dist;
-            dist = Mathf.Sqrt(Mathf.Pow(xp-xc, 2) + Mathf.Pow(yp-yc, 2));   
-            Vector2 V = GetComponent<Rigidbody2D>().velocity;
-            if (dist > Radius) {
-                V = new Vector2((xo-xc), (yo-yc))*Elasticity;
-            }
-            else {
-                V = new Vector2(0, 0);
-            }
             GetComponent<Rigidbody2D>().velocity = V;
         }
         else {
-            transform.position = Target.transform.position + new Vector3(0, 0, -10);
+            if (dist > Radius) {
+                transform.position = new Vector3(xo, yo, 0) + new Vector3(0, 0, -10);
+            }
         }
     }
 }
--- Assets/Scripts/PlayerControler.cs
+++ Assets/Scripts/PlayerControler.cs
@@ -5,12 +5,19 @@ using System;
 
 public class PlayerControler : MonoBehaviour
 {
+    public float Speed;
     public float Acceleration;
     public bool Gravity;
+    public float JumpForce;
+
+    private float[] ForcePull;
     // Start is called before the first frame update
     void Start()
     {
-        Gravity = GetComponent<Rigidbody2D>().gravityScale != 0;
+        ForcePull = new float[2];
+        ForcePull[0] = 0;
+        ForcePull[1] = 0;
+        //Gravity = GetComponent<Rigidbody2D>().gravityScale != 0;
     }
 
     // Update is called once per frame
@@ -21,28 +28,115 @@ public class PlayerControler : MonoBehaviour
 
     void Update()
     {
-        if (GetComponent<Rigidbody2D>().position.y < 0) {
+        /*if (GetComponent<Rigidbody2D>().position.y < 0) {
             GetComponent<Rigidbody2D>().position = new Vector2(GetComponent<Rigidbody2D>().position.x, 0);
-        }
+        }*/
 
         float x,y;
         x = Input.GetAxis("Horizontal");
         y = Input.GetAxis("Vertical");
-        
+
         if (Gravity) {
             y = 0;
+            if (Input.GetAxis("Jump") != 0 && isGrounded()) {
+                //GetComponent<Rigidbody2D>().AddForce(new Vector2(0, JumpForce));
+                y = JumpForce;
+            }
         }
 
-        
-        Vector2 contr = new Vector2(x, y);
+        Vector2 control = new Vector2(x, y);
+
+        Vector2 current_velocity = GetComponent<Rigidbody2D>().velocity;
+
+        Vector2 new_velocity = GetComponent<Rigidbody2D>().velocity;
+
+        //if (current_velocity.magnitude <= Speed && control.magnitude > 0) {
+        //    GetComponent<Rigidbody2D>().velocity = control * Speed;
+        //}
+        if ((Mathf.Abs(current_velocity.x) <= Mathf.Abs(control.x) * Speed || control.x * current_velocity.x < 0) && ForcePull[0] == 0) {
+            new_velocity.x = control.x * Speed;
+        }
+        else {
+                if (ForcePull[0] == 0 && (Mathf.Abs(current_velocity.x) > Speed)) {
+                    ForcePull[0] = current_velocity.x / Mathf.Abs(current_velocity.x);
+                }
+
+                if (ForcePull[0] * (current_velocity.x - control.x * Speed) <= 0 && ForcePull[0] != 0) {
+                    ForcePull[0] = 0;
+                    new_velocity.x = control.x * Speed;
+                }
+                
+                
+                if (((ForcePull[0] * (current_velocity.x - control.x * Speed) > 0 && ForcePull[0] * control.x < 0)) && ForcePull[0] != 0) {
+                    //new_velocity.x += control.x * Acceleration;
+                    GetComponent<Rigidbody2D>().AddForce(new Vector2(Acceleration * control.x, 0));
+                }
+                
+        }
+
+        if ((Mathf.Abs(current_velocity.y) <= Mathf.Abs(control.y) * Speed || control.y * current_velocity.y < 0) && ForcePull[1] == 0) {
+            new_velocity.y = control.y * Speed;
+        }
+        else {
+                if (ForcePull[1] == 0 && (Mathf.Abs(current_velocity.y) > Speed)) {
+                    ForcePull[1] = current_velocity.y / Mathf.Abs(current_velocity.y);
+                }
+
+                if (ForcePull[1] * (current_velocity.y - control.y * Speed) <= 0 && ForcePull[1] != 0) {
+                    ForcePull[1] = 0;
+                    new_velocity.y = control.y * Speed;
+                }
+                
+                
+                if (((ForcePull[1] * (current_velocity.y - control.y * Speed) > 0 && ForcePull[1] * control.y < 0)) && ForcePull[1] != 0) {
+
+                    GetComponent<Rigidbody2D>().AddForce(new Vector2(0, Acceleration * control.y));
+                }
+                
+        }
+
+        GetComponent<Rigidbody2D>().velocity = new_velocity;
+        //Vector2 contr = new Vector2(x, y) * Speed;
         //GetComponent<Rigidbody2D>().velocity += contr*Acceleration;
-        GetComponent<Rigidbody2D>().AddForce(contr*Acceleration);
+        //GetComponent<Rigidbody2D>().velocity += contr;
     }
 
 
 
-
-
-
+    public Transform GroundCheck;
+    public LayerMask GroundLayer;
+    bool isGrounded() {
+        bool onGround = Physics2D.OverlapCircle(GroundCheck.position, GroundCheck.GetComponent<CircleCollider2D>().radius, GroundLayer);
+        return onGround;
+    }
 
 }
+/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class PlayerControler : MonoBehaviour
+{
+    public float Speed;
+    public float moveG;
+    public float moveV;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveG = Input.GetAxis("Horizontal") * Speed;
+        moveV = Input.GetAxis("Vertical") * Speed;
+        GetComponent<Rigidbody2D>().velocity = new Vector2(moveG, moveV); 
+    }
+}
+
+
+
+
+*/
